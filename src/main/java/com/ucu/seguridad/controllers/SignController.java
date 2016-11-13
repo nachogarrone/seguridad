a623de82f91b6eb9fff605bebd166d21ce9405d6
Alfonso <Alfonso@mbp-de-alfonso>
2016-11-13 20:02:40 -0300

    Incorporando cédula

--- src/main/java/com/ucu/seguridad/controllers/SignController.java
+++ src/main/java/com/ucu/seguridad/controllers/SignController.java
@@ -28,6 +28,26 @@ import java.security.cert.Certificate;
 import java.security.cert.CertificateException;
 import java.util.NoSuchElementException;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.security.Key;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.Provider;
+import java.security.Security;
+import java.security.KeyStore.CallbackHandlerProtection;
+import java.security.cert.Certificate;
+import java.security.cert.X509Certificate;
+import java.util.Enumeration;
+import java.util.Properties;
+
+import javax.security.auth.callback.*;
+
+import sun.security.pkcs11.SunPKCS11;
+
 /**
  * Created by nachogarrone on 10/29/16.
  */
@@ -117,9 +137,9 @@ public class SignController {
         ClassLoader classLoader = getClass().getClassLoader();
 //        File file = new File(classLoader.getResource("test.p12").getFile());
 //        readPrivateKeyFromPKCS12(file, "jPdfSign");
-        File file = new File(classLoader.getResource("test2.p12").getFile());
-        readPrivateKeyFromPKCS12(file, "test");
-
+        //File file = new File(classLoader.getResource("test2.p12").getFile());
+        //readPrivateKeyFromPKCS12(file, "test"); En el main obtniene privatekey y certificado
+        
         PdfReader reader = new PdfReader(fileToSign.getAbsolutePath());
         FileOutputStream fout = new FileOutputStream("Prueba-firmada.pdf");
 
@@ -132,6 +152,64 @@ public class SignController {
         sap.setVisibleSignature(new Rectangle(100, 100, 200, 200), 1, null);
         stamper.close();
     }
+    
+    public static KeyStore loadKeystore(CallbackHandler passwordCallbackHandler) throws IOException {
+		try {
+			
+			Provider pkcs11Prov = createPkcs11Provider();
+			CallbackHandlerProtection pwCallbackProt = new CallbackHandlerProtection(passwordCallbackHandler);
+			KeyStore.Builder builder = KeyStore.Builder.newInstance("PKCS11", pkcs11Prov, pwCallbackProt);
+			return builder.getKeyStore();
+		} catch (KeyStoreException e) {
+			throw new RuntimeException("Failed to load PKCS#11 Keystore",e);
+		}
+	}
+    
+    private static Provider createPkcs11Provider() throws IOException {
+		SunPKCS11 result = new sun.security.pkcs11.SunPKCS11("C:\\Users\\alfonso\\workspace32\\Seguridad\\ConfigAux.cfg");
+		if (result.getService("KeyStore", "PKCS11") == null) {
+			throw new RuntimeException("No PKCS#11 Service available. Probably Security Token (Smartcard) not inserted");
+		}
+		System.out.println("sali del new SunPKCS11");
+		// Register the Provider 
+		if (Security.getProvider(result.getName()) != null) {
+			Security.removeProvider(result.getName());
+		}
+		Security.addProvider(result);			
+		return result;
+	}
+    public static void main(String[] args) throws Exception {
+		// Test for the DataKey 330 Smartcard 
+		// (dskck201.dll is installed with CIP Utilities from DataKey)
+		SwingPasswordCallbackHandler swing = new SwingPasswordCallbackHandler();
+		KeyStore ks = loadKeystore(swing );
+		
+		for(Enumeration<String> aliases = ks.aliases(); aliases.hasMoreElements(); ) {
+			String alias = aliases.nextElement();
+			System.out.println(alias);
+			
+			// print certifcate
+			Certificate cert = ks.getCertificate(alias);
+			certificateChain = ks.getCertificateChain(alias);
+			if (cert != null) {
+				System.out.print(" Certificate found. type="+cert.getType());
+				if (cert instanceof X509Certificate) {
+					X509Certificate x509 = (X509Certificate)cert;
+					System.out.print(" SubjectDN="+x509.getSubjectDN());
+					certificateChain
+				}
+				System.out.println();
+			}
+			
+			// private key is accessed without password 
+			privateKey = ks.getKey(alias, null);
+			if (pk != null) {
+				System.out.println(" Private key found. algorithm="+pk.getAlgorithm());
+			}
+			
+			System.out.println();
+		}
+	}
 
 
 }
